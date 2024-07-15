c4808427b3ae543bb9243d8cfdb4c5f00785261f
Ashish Dung Dung <ashishdungdung@outlook.com>
2024-07-16 04:19:00 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: discourse-plugin-cateogry-terminology
+# about: Changes "category" and "categories" to "hub" and "hubs" or anything else
+# version: 0.1
+# authors: Ashish Dungdung
+
+register_asset "javascripts/discourse-plugin-hubterminology.js"
+
+after_initialize do
+  # Add initialization code here if needed
+end
